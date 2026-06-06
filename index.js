f1ab1ea683a2c3b59cd48febe9e74fc393862a4a
adnaan299 <adnaanansari299@gmail.com>
2026-06-06 18:04:12 +0530

    Design database schema ✅

---
+++ index.js
@@ -0,0 +1,52 @@
+// what are the data that we need to store
+// username, password -> whenever the users signs in we will populate the users array
+// organization -> users creates the organization it populates the organization array
+// boards -> when user creates boards it is going to populate the boards array
+// issues -> populates issues array
+
+// users = [
+//     {
+//         if : 1,
+//         username : "adnaan",
+//         password : "123123"
+//     }
+// ]
+
+// const organizations = [
+//     {
+//         id : 1,
+//         title : "zomato",
+//         description : "Food platform",
+//         admin : 1,
+//         members : [2]
+//     }
+// ];
+
+// const boards = [
+//     {id: 1,
+//     title: "zomato website(frontend)",
+//     organizations: 1,
+//     }
+// ];
+
+// const issues = [
+//     {
+//         id: 1,
+//         title: "Add dark mode",
+//         boardId : 1
+//     },
+//     {
+//         id: 2,
+//         title: "fix random bug",
+//         boardId : 1
+//     },
+
+// ];
+
+
+
+const users = [];
+const organizations = [];
+const boards = [];
+const issues = [];
+
